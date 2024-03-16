fc6159380f0c101e069a8a8001c4e2ec62d764b0
Nguyễn Huyền Trang <63178183+Peaceminusone1203@users.noreply.github.com>
2024-03-16 14:44:29 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
 public class Main {
+
+    private static final int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
+
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
 
+        final int[] sum = {0};
+        final int[] max = {Integer.MIN_VALUE};
 
-        Viết mã của bạn vào đây
-    
+        Thread sumThread = new Thread(() -> {
+            for (int number : numbers) {
+                sum[0] += number;
+            }
+        });
+
+        Thread maxThread = new Thread(() -> {
+            for (int number : numbers) {
+                if (number > max[0]) {
+                    max[0] = number;
+                }
+            }
+        });
+
+        sumThread.start();
+        maxThread.start();
+
+        try {
+            sumThread.join();
+            maxThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Phần tử lớn nhất trong mảng là: " + max[0]);
+        System.out.println("Tổng của các phần tử trong mảng là: " + sum[0]);
     }
 }
